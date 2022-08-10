a7b2b3046c7560e2ed6e56e55d0aa7797d678bbc
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-08-10 20:18:03 +0200

    Create input2.c

---
+++ input2.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+/**
+ * read_line - function that get the user input
+ * void: n/a
+ * Return: The user command
+ */
+char *read_line(void)
+{
+	size_t buffersize = 0;
+	int readline_status = 0;
+	char *buff = NULL;
+
+	readline_status = getline(&buff, &buffersize, stdin);
+	if (readline_status == -1)
+	{
+		_putchar('\n');
+		free(buff);
+		exit(0);
+	}
+	buff[readline_status - 1] = '\0';
+	return (buff);
+}
