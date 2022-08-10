d8036978b1334c2acfb7e50f736d0460b4ca9f6e
RazaanD <104936934+RazaanD@users.noreply.github.com>
2022-08-10 20:10:36 +0200

    Create argu.c

---
+++ argu.c
@@ -0,0 +1,21 @@
+#include "../shell.h"
+/**
+ * main - Prints all the arguments without strok
+ * @argc: take an argument.
+ * @argv: take an argument.
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	(void) argc;
+
+	while (*argv != NULL)
+	{
+		printf("argv[%d] = %s \n", i, *argv);
+		argv++;
+		i++;
+	}
+
+	return (0);
+}
